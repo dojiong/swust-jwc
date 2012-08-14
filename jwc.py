465137331b62237a2fe649d72749418431b5c1d4
lo <lodevil@live.cn>
2012-08-14 23:04:52 +0800

    modify urlopen method

--- jwc.py
+++ jwc.py
@@ -32,6 +32,24 @@ class JWC(object):
     def cookie_url(self, u):
         return self._cookieurl + u
 
+    def _page(self, page_name, get_args=None, post_args=None):
+        url = self.cookie_url(self.links[page_name])
+        if get_args is not None:
+            url = '%s?%s' % (url, urllib.urlencode(get_args))
+        if post_args:
+            req = urllib2.Request(url, urllib.urlencode(post_args),
+                {'Referer': self._main_url})
+        else:
+            req = urllib2.Request(url, None,
+                {'Referer': self._main_url})
+        return urllib2.urlopen(req)
+
+    def get_page(self, page_name, **kwargs):
+        return self._page(page_name, get_args=kwargs)
+
+    def post_page(self, page_name, **kwargs):
+        return self._page(page_name, post_args=kwargs)
+
     def login(self, username, password):
         data = self.open()
         post = urllib.urlencode({
@@ -57,23 +75,17 @@ class JWC(object):
             return True
         return False
 
-    def get_scores(self, xn='', xq=''):
-        url = self.cookie_url(self.links[u'个人成绩查询'])
-        req = urllib2.Request(url, None,
-            {'Referer': self._main_url})
-        rep = urllib2.urlopen(req)
-        data = rep.read()
+    def get_scores(self, school_year='', term=''):
+        data = self.get_page(u'个人成绩查询').read()
         post = {'__VIEWSTATE': self._get_view_state(data),
-            'ddlXN': xn, 'ddlXQ': xq}
-        if not xn:
+            'ddlXN': school_year, 'ddlXQ': term}
+        if not school_year:
             post['Button2'] = urllib.quote(u'在校学习成绩查询'.encode('gbk'))
-        elif not xq:
+        elif not term:
             post['Button5'] = urllib.quote(u'按学年查询'.encode('gbk'))
         else:
             post['Button1'] = urllib.quote(u'按学期查询'.encode('gbk'))
-        req = urllib2.Request(url, urllib.urlencode(post),
-                {'Referer': url})
-        rep = urllib2.urlopen(req)
+        rep = self.post_page(u'个人成绩查询', **post)
         data = rep.read().decode('gbk')
         table = data[data.find('<table'): data.find('</table>')]
         trs = table.split('<tr')[2:]
@@ -82,8 +94,8 @@ class JWC(object):
             cols = re.findall(r'<td>([^<]*)</td>', tr)
             scores.append({
                 'name': cols[3],
-                'xf': cols[6],
-                'jd': cols[7],
+                'credit': cols[6],
+                'grade_point': cols[7],
                 'score': cols[8]
                 })
         avg_i = data.find(u'平均学分绩点：') + len(u'平均学分绩点：')
@@ -97,5 +109,5 @@ if __name__ == '__main__':
     if jwc.login(username, password):
         scores = jwc.get_scores('2011-2012', '2')
         for score in scores['scores']:
-            print score['name'], score['xf'], score['jd'], score['score']
+            print score['name'], score['credit'], score['grade_point'], score['score']
         print u'平均学分绩点:', scores['avg']
